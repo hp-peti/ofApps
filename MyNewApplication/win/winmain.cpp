83b5d0e6096ea0168c3899b97bb0d2aef214d71d
Hanos-Puskai Peter <hp.peti@gmail.com>
2016-04-10 17:30:53 +0300

    Added support for non-console windows app in Release mode

---
+++ MyNewApplication/win/winmain.cpp
@@ -0,0 +1,13 @@
+#include <Windows.h>
+
+extern int main();
+
+int CALLBACK WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_ HINSTANCE hPrevInstance,
+	_In_ LPSTR     lpCmdLine,
+	_In_ int       nCmdShow
+)
+{
+	return main();
+}
