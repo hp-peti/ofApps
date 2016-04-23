9611587fc411f7c999787d6131e3ddf17616e978
Hanos-Puskai Peter <hp.peti@gmail.com>
2016-04-24 01:07:55 +0300

    Source Formatting

--- MyNewApplication/src/Transition.h
+++ MyNewApplication/src/Transition.h
@@ -11,69 +11,66 @@
 #include <chrono>
 #include <functional>
 
-struct TransitionBase
-{
-	using Timestamp = std::chrono::system_clock::time_point;
-	using TimeDiff = std::chrono::system_clock::duration;
+struct TransitionBase {
+    using Timestamp = std::chrono::system_clock::time_point;
+    using TimeDiff = std::chrono::system_clock::duration;
 
-	using Milliseconds = std::chrono::duration<float, std::milli>;
+    using Milliseconds = std::chrono::duration<float, std::milli>;
 
-	static Timestamp clockNow () {
-		return std::chrono::system_clock::now();
-	}
+    static Timestamp clockNow() {
+        return std::chrono::system_clock::now();
+    }
 };
 
 template <class Scalable>
-struct Transition: TransitionBase
-{
-	std::function< Scalable () > getValue;
-	std::function< float () > getInterval;
+struct Transition: TransitionBase {
+    std::function<Scalable()> getValue;
+    std::function<float()> getInterval;
 
-	template <typename Value, typename Interval>
-	Transition(Value value, Interval interval)
-		: getValue(value)
-		, getInterval(interval)
-	{
-		using namespace std::chrono;
-		beginValue = getValue();
-		startTime = system_clock::now();
-		lastTime = startTime;
-		next();
-	}
+    template <typename Value, typename Interval>
+    Transition(Value value, Interval interval) :
+        getValue(value),
+        getInterval(interval) {
+        using namespace std::chrono;
+        beginValue = getValue();
+        startTime = system_clock::now();
+        lastTime = startTime;
+        next();
+    }
 
-	void update(Timestamp now)
-	{
-		if (now == lastTime)
-			return;
-		lastTime = now;
-		TimeDiff elapsed = now - startTime;
-		if (elapsed > transitionLength) {
-			currentValue = endValue;
-			beginValue = endValue;
-			startTime = lastTime;
-			next();
-		} else {
-			float mix = elapsed / transitionLength;
-			currentValue = beginValue * (1-mix) + endValue * mix;
-		}
-	}
+    Transition(const Transition &) = default;
 
-	Scalable get() const {
-		return currentValue;
-	}
+    void update(Timestamp now) {
+        if (now == lastTime)
+            return;
+        lastTime = now;
+        TimeDiff elapsed = now - startTime;
+        if (elapsed > transitionLength) {
+            currentValue = endValue;
+            beginValue = endValue;
+            startTime = lastTime;
+            next();
+        } else {
+            float mix = elapsed / transitionLength;
+            currentValue = beginValue * (1 - mix) + endValue * mix;
+        }
+    }
 
+    Scalable get() const {
+        return currentValue;
+    }
 
 private:
-	void next() {
-		transitionLength = Milliseconds(getInterval());
-		endValue = getValue();
-	}
+    void next() {
+        transitionLength = Milliseconds(getInterval());
+        endValue = getValue();
+    }
 
-	Scalable beginValue, endValue;
-	Timestamp startTime;
-	Timestamp lastTime;
-	Milliseconds transitionLength;
-	Scalable currentValue;
+    Scalable beginValue, endValue;
+    Timestamp startTime;
+    Timestamp lastTime;
+    Milliseconds transitionLength;
+    Scalable currentValue;
 
 };
 
